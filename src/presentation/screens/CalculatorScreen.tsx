27754e4f40c64ec404319464f8a26f78c409560b
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-01-11 10:37:55 -0600

    Botones y estilos

--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
 import {globalStyles} from '../../config/theme/app-theme';
 
 export const CalculatorScreen = () => {
@@ -9,6 +9,21 @@ export const CalculatorScreen = () => {
       <View style={{paddingHorizontal: 30, paddingVertical: 20}}>
         <Text style={globalStyles.mainResult}>1500</Text>
         <Text style={globalStyles.subResult}>15</Text>
+      </View>
+
+      <View style={globalStyles.row}>
+        <Pressable style={globalStyles.button}>
+          <Text style={globalStyles.buttonText}>Hola Mundo</Text>
+        </Pressable>
+        <Pressable style={globalStyles.button}>
+          <Text style={globalStyles.buttonText}>Hola Mundo</Text>
+        </Pressable>
+        <Pressable style={globalStyles.button}>
+          <Text style={globalStyles.buttonText}>Hola Mundo</Text>
+        </Pressable>
+        <Pressable style={globalStyles.button}>
+          <Text style={globalStyles.buttonText}>Hola Mundo</Text>
+        </Pressable>
       </View>
     </View>
   );
